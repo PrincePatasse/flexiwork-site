e77807815951df340cef9d86b3e8cfb3c7bee01b
PrincePatasse <prince.patasse@flexiwork.fr>
2026-01-01 20:59:46 +0100

    Retour version stable MixpanelProvider

--- app/MixpanelProvider.js
+++ app/MixpanelProvider.js
@@ -1,31 +1,18 @@
 'use client'
 
-import { useEffect, Suspense } from 'react'
-import { usePathname, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+import { usePathname } from 'next/navigation'
 import { initMixpanel, track } from '@/lib/mixpanel'
 
-function MixpanelTracker() {
+export default function MixpanelProvider() {
   const pathname = usePathname()
-  const searchParams = useSearchParams()
 
   useEffect(() => {
     initMixpanel()
-    
-    // Capture UTM parameters
-    const utmSource = searchParams?.get('utm_source')
-    const utmCampaign = searchParams?.get('utm_campaign')
-    const utmContent = searchParams?.get('utm_content')
-    
-    const utmParams = {}
-    if (utmSource) utmParams.utm_source = utmSource
-    if (utmCampaign) utmParams.utm_campaign = utmCampaign
-    if (utmContent) utmParams.utm_content = utmContent
-    
     track('Session Start', { 
-      entry_page: pathname,
-      ...utmParams
+      entry_page: pathname
     })
-  }, [pathname, searchParams])
+  }, [])
 
   useEffect(() => {
     track('Page View', { 
@@ -35,12 +22,4 @@ function MixpanelTracker() {
   }, [pathname])
 
   return null
-}
-
-export default function MixpanelProvider() {
-  return (
-    <Suspense fallback={null}>
-      <MixpanelTracker />
-    </Suspense>
-  )
 }
